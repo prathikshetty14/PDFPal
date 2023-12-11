475c8350fa71be29cc0ffb51cf8aa05222d7ca89
prathikshetty14 <prathikshetty1411@gmail.com>
2023-12-11 19:45:36 +0530

    Created File Uploader and set up UploadThing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,49 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { 
+    createUploadthing, 
+    type FileRouter 
+} from "uploadthing/next";
+ 
+const f = createUploadthing();
+ 
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+ 
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const {getUser} = getKindeServerSession();
+      const user = await getUser();
+ 
+      // If you throw, the user will not be able to upload
+      if (!user || !user.id) throw new Error("Unauthorized");
+ 
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`, // or file.url
+          uploadStatus: 'PROCESSING',
+        }
+      })
+
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+ 
+      console.log("file url", file.url);
+ 
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
