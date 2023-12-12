e71cddd514c82d791a9ed98df13dcbbf37eb4de8
prathikshetty14 <prathikshetty1411@gmail.com>
2023-12-12 14:18:07 +0530

    Initialised Zooming feature along with drop down in the top bar of the PDF file

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2, Search } from "lucide-react";
 import {Document, Page, pdfjs} from "react-pdf";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -15,6 +15,9 @@ import { z } from "zod";
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { cn } from "@/lib/utils";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+import SimpleBar from "simplebar-react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -29,7 +32,9 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
     const {width, ref} = useResizeDetector();
     const [numPages, setNumPages] = useState<number>();
     const [currPage, setCurrPage] = useState<number>(1);
-    const [isInputFocused, setIsInputFocused] = useState(false);
+    const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
+
+    const [scale, setScale] = useState<number>(1)
 
     const handleInputFocus = () => {
         setIsInputFocused(true);
@@ -111,30 +116,65 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
                     <ChevronUp className="h-4 w-4"/>
                 </Button>
             </div>
+
+            <div className="space-x-2">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button 
+                        className="gap-1.5"
+                        aria-label="zoom" 
+                        variant="ghost"
+                        >
+                            <Search className="h-4 w-4"/>
+                            {scale * 100}%<ChevronDown className="h-3 w-3 opacity-50"/>
+                        </Button>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent>
+                        <DropdownMenuItem onSelect={() => setScale(1)}>
+                            100%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                            150%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setScale(2)}>
+                            200%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setScale(2.5)}>
+                            250%
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
 
         <div className="flex-1 w-full max-h-screen">
-            <div ref={ref}>
-                <Document
-                loading = {
-                    <div className="flex justify-center">
-                        <Loader2 className="my-24 h-6 w-6 animate-spin" />
-                    </div>
-                }
-                onLoadError={() => {
-                    toast({
-                        title: 'Error loading PDF',
-                        description: 'Please try again later',
-                        variant: 'destructive'
-                    })
-                }} 
-                onLoadSuccess={({numPages}) => setNumPages(numPages)}
-                file={url} 
-                className="max-h-full">
-                    <Page width={width ? width : 750}  
-                    pageNumber={currPage}/>
-                </Document>
-            </div>
+            <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
+                <div ref={ref}>
+                    <Document
+                    loading = {
+                        <div className="flex justify-center">
+                            <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                        </div>
+                    }
+                    onLoadError={() => {
+                        toast({
+                            title: 'Error loading PDF',
+                            description: 'Please try again later',
+                            variant: 'destructive'
+                        })
+                    }} 
+                    onLoadSuccess={({numPages}) => setNumPages(numPages)}
+                    file={url} 
+                    className="max-h-full">
+                        <Page 
+                        width={width ? width : 750}  
+                        pageNumber={currPage}
+                        scale={scale}
+                        />
+                    </Document>
+                </div>
+            </SimpleBar>
         </div>
     </div>
     )
