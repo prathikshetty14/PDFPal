91a2f954b9b299c08bb34bf763f17dd2a9fd7eef
prathikshetty14 <prathikshetty1411@gmail.com>
2023-12-12 13:47:46 +0530

    Initialised Input field with buttons to access page numbers in the top bar of the PDF file

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,11 +1,20 @@
 "use client"
 
-import { Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import {Document, Page, pdfjs} from "react-pdf";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { useToast } from "./ui/use-toast";
 import {useResizeDetector} from "react-resize-detector";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
+
+import {useForm} from "react-hook-form"
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { cn } from "@/lib/utils";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -17,14 +26,90 @@ interface PdfRendererProps {
 const PdfRenderer = ({url}: PdfRendererProps) => {
     
     const {toast} = useToast();
+    const {width, ref} = useResizeDetector();
+    const [numPages, setNumPages] = useState<number>();
+    const [currPage, setCurrPage] = useState<number>(1);
+    const [isInputFocused, setIsInputFocused] = useState(false);
 
-    const {width, ref} = useResizeDetector()
+    const handleInputFocus = () => {
+        setIsInputFocused(true);
+    };
+    
+    const handleInputBlur = () => {
+        setIsInputFocused(false);
+    };
+
+    const CustomPageValidator = z.object({
+        page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
+    })
+
+    type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue
+    } = useForm<TCustomPageValidator>({
+        defaultValues: {
+            page: "1"
+        },
+        resolver: zodResolver(CustomPageValidator)
+    })
+
+    const handlePageSubmit = ({
+        page,
+    }: TCustomPageValidator) => {
+        setCurrPage(Number(page));
+        setValue("page", String(page))
+    } 
 
     return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
         <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
             <div className="flex items-center gap-1.5">
-                Top Bar
+                <Button
+                disabled={currPage <= 1}
+                onClick={() =>  {
+                    setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+                }} 
+                variant="ghost" 
+                aria-label="previous page"
+                >
+                    <ChevronDown className="h-4 w-4"/>
+                </Button>
+
+
+                <div className="flex items-center gap-1.5">
+                    <Input 
+                    {...register("page")}
+                    className={cn("w-12 h-8", errors.page && "focus-visible:ring-red-500")}
+                    onFocus={handleInputFocus}
+                    onBlur={handleInputBlur}
+                    onKeyDown={(e) => {
+                        if(e.key === "Enter"){
+                            handleSubmit(handlePageSubmit)();
+                        }
+                    }}
+                    value={isInputFocused ? undefined : String(currPage)}
+                    />
+                    <p className="text-zinc-700 text-sm space-x-1">
+                        <span>/</span>
+                        <span>{numPages ?? "x"}</span>
+                    </p>
+                </div>
+
+
+                <Button
+                disabled={numPages === undefined || currPage === numPages}
+                onClick={() =>  {
+                    setCurrPage((prev) => prev + 1 > numPages! ? numPages! : prev + 1);
+                }} 
+                variant="ghost" 
+                aria-label="previous page"
+                >
+                    <ChevronUp className="h-4 w-4"/>
+                </Button>
             </div>
         </div>
 
@@ -43,9 +128,11 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
                         variant: 'destructive'
                     })
                 }} 
+                onLoadSuccess={({numPages}) => setNumPages(numPages)}
                 file={url} 
                 className="max-h-full">
-                    <Page width={width ? width : 1}  pageNumber={1}/>
+                    <Page width={width ? width : 750}  
+                    pageNumber={currPage}/>
                 </Document>
             </div>
         </div>
